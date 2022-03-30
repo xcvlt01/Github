a9aa38cdb6a7b1d1de9a6384a11fe28d83bb13e8
Zsofie <zsofia@linumlabs.com>
2022-03-30 06:54:18 +0200

    adding new properties

--- objects.js
+++ objects.js
@@ -6,16 +6,32 @@
 
 // _______DECLARE AN OBJECT_______ //
 
-const object = {
+const Object = {
   property1,
   property2,
   property3,
-  property4,
 };
 
 // example
-const person = {
+const Person = {
   name: "Marci",
   age: 30,
   job: "future solidity dev",
 };
+
+// _______READING A PROPERTY VALUE FROM AN OBJECT_______ //
+
+const personName = person.name;
+console.log(personName); // this will return "Marci"
+
+const personAge = person.age;
+console.log(personAge); // this will return 30
+
+const personJob = person.job;
+console.log(personJob); // this will return "future solidity dev"
+
+// _______ADD NEW PROPERTY TO OBJECT_______ //
+
+// you can add new properties to an object that already exist
+Person.isHuman = false;
+console.log(Person); // this should return {name:"Marci",age:30,job:"future solidity dev",isHuman:false}

1fbf507a6a8887c7037b7345ac5eb14c07a5eeea
Zsofie <zsofia@linumlabs.com>
2022-03-30 06:40:06 +0200

    added declare an object

--- objects.js
+++ objects.js
@@ -3,3 +3,19 @@
 // Variables are containers for data values.
 // An object is a variable that can have different properties and these properties can have different values.
 //The values are written as name:value pairs.
+
+// _______DECLARE AN OBJECT_______ //
+
+const object = {
+  property1,
+  property2,
+  property3,
+  property4,
+};
+
+// example
+const person = {
+  name: "Marci",
+  age: 30,
+  job: "future solidity dev",
+};

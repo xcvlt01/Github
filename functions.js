4126a5ef1d65b415e5e20a2c09d98fdb6760fbe7
Zsofie <zsofia@linumlabs.com>
2022-03-30 16:24:55 +0200

    added exercise

--- functions.js
+++ functions.js
@@ -20,3 +20,12 @@ function addNumbers() {
 }
 
 setAndAddNumbers(1, 3); // when you call this function your result in the console will be 4
+
+/// *** EXERCISE 1 *** ///
+
+// 1. Declare 3 global variables (drink1,drink2,drink3)
+// 2. Declare a function called setValuesAndCreateArray()
+// 3. Declare another function called CreateArray()
+// 4. CreateArray() should push all of your variables into an array and log the array in the console.
+// 5. setValuesAndCreateArray() should set the values of the variables and should call CreateArray() underneath
+// 6. call setValuesAndCreateArray() and check the console

26f6f663745218dbba0296344e49be5238dd3595
Zsofie <zsofia@linumlabs.com>
2022-03-30 06:33:38 +0200

    added objects.js

---
+++ objects.js
@@ -0,0 +1,5 @@
+// _______OBJECTS_______ //
+
+// Variables are containers for data values.
+// An object is a variable that can have different properties and these properties can have different values.
+//The values are written as name:value pairs.
